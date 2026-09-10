74457e456ddd1d5690c1c8b968644a1091b5ddca
Vineet Yadav <vineetyadavheer@gmail.com>
2026-09-10 19:03:28 +0530

    Updating the model

--- src/training.py
+++ src/training.py
@@ -1,20 +1,22 @@
-import os
+from pathlib import Path
 import pandas as pd
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
-BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_PATH=os.path.join(BASE_DIR,'data','creditcard.csv')
-MODEL_PATH=os.path.join(BASE_DIR,'model.pkl')
+BASE_DIR=Path(__file__).resolve().parent.parent
+DATA_PATH=BASE_DIR/'data'/'creditcard.csv'
+MODEL_PATH=BASE_DIR/'model.pkl'
 
 data=pd.read_csv(DATA_PATH)
+
+
 X=data.drop(columns=['Class'])
 y=data['Class']
 
 model=RandomForestClassifier(n_estimators=200,
-                             max_features='sqrt',
-                             random_state=42,
-                             max_depth=None)
+                            random_state=42,
+                            max_depth=None)
+                             
 
 model.fit(X,y)
 
